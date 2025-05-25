3ab45e4070392435784c24fde71f854fdda542ee
Parthiban-D-89 <parthi89it@gmail.com>
2025-05-25 13:00:06 +0530

    Add files via upload

---
+++ week1/day1/chrome.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class chrome {
+
+	public static void main(String[] args) {
+
+		float version = 91.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int releaseYear = 2008;
+		char shortcutKey = 'C';
+
+		System.out.println("Google Chrome Version is " + version);
+		System.out.println("Application Name " + developer);
+		System.out.println("Is this one a Beta Version " + isBeta);
+		System.out.println("ReleaseYear " + releaseYear);
+		System.out.println("Shortcutkey for launching " + shortcutKey);
+
+	}
+
+}
